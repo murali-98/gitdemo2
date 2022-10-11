ff813d0c4d6d41d5c91b545d3090e7bfed35b697
mbr6 <mbr6@IN-5CG00203M5>
2022-10-11 16:43:31 +0530

    first change

---
+++ githubdemo1/src/com/dxc/githubdemo1/GitExample.java
@@ -0,0 +1,12 @@
+package com.dxc.githubdemo1;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20,c=a+b;
+		System.out.println(c);
+	}
+
+}
