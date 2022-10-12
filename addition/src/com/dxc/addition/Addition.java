d0584b22853b6922be42fc6713680e5174f4fb17
B R <murali.br@dxc.com>
2022-10-12 13:33:29 +0530

    update git proj

--- addition/src/com/dxc/addition/Addition.java
+++ addition/src/com/dxc/addition/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int a=10,b=20;
+     int a=1,b=2;
      int c=a+b;
      System.out.println("the sum of a and b is:"+ c);
 	}
