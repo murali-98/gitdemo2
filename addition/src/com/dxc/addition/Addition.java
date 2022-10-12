3cdfd1ec699a5059c4a4226ad00b9123722f0533
B R <murali.br@dxc.com>
2022-10-12 13:12:36 +0530

    second commit

---
+++ addition/src/com/dxc/addition/Addition.java
@@ -0,0 +1,12 @@
+package com.dxc.addition;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10,b=20;
+     int c=a+b;
+     System.out.println("the sum of a and b is:"+ c);
+	}
+
+}
